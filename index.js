ab378f3f7b177c2552ad32723a740349e662080b
Zulai Romero <zulairomero@gmail.com>
2023-07-23 20:55:15 -0400

    Add event listener to die for generating workout, configure clear circuit button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 
+// EVENT LISTENER HOOKS UP ROLL DIE UP WITH GENERATOR
+document.querySelector(`#roll-die`).addEventListener(`click`, () => {
 // GRABS WORKOUT DATABASE AND GENERATES A RANDOM WORKOUT PROPERTY FROM THE ARRAY
 fetch("http://localhost:3000/workouts")
 .then(res => res.json())
@@ -9,5 +11,27 @@ const getRandomWorkout = (workOuts) => {
         return null;
     }
     const randomIndex = Math.floor(Math.random() * workOuts.length);
-    return workOuts[randomIndex];
+    const generatedWorkOut = workOuts[randomIndex];
+    const circuitList = document.querySelector(`#current-circuit`)
+    const newP = document.createElement(`li`)
+    newP.innerText = generatedWorkOut.workout
+    newP.setAttribute(`class`, `current`)
+    circuitList.appendChild(newP)
 }
+})
+
+const archiveButton = document.querySelector(`#save-circuit`)
+archiveButton.addEventListener(`click`, () => {
+    console.log(`i am archived!`)
+})
+
+
+// CLEARS CURRENT CIRCUIT
+const clearButton = document.querySelector(`#clear-circuit`)
+clearButton.addEventListener(`click`, () => {
+    console.log(`i am cleared!`)
+    const circuitList = document.querySelector(`#current-circuit`)
+    circuitList.innerText = ``
+})
+
+

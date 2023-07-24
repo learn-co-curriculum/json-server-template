6300b787e800f6486e1d68e4bd0594ee80f802e6
Zulai Romero <zulairomero@gmail.com>
2023-07-24 11:06:31 -0400

    Update db, src gifs, and archive circuit functionality

--- index.js
+++ index.js
@@ -14,24 +14,49 @@ const getRandomWorkout = (workOuts) => {
     const generatedWorkOut = workOuts[randomIndex];
     const circuitList = document.querySelector(`#current-circuit`)
     const newP = document.createElement(`li`)
+    const workoutImg = document.createElement(`img`)
+    workoutImg.style.height = `200px`
+    workoutImg.src = generatedWorkOut.img
+    circuitList.append(workoutImg)
     newP.innerText = generatedWorkOut.workout
     newP.setAttribute(`class`, `current`)
     circuitList.appendChild(newP)
 }
+
 })
 
+
+// CLEARS CURRENT CIRCUIT BUTTON
+const circuitList = document.body.querySelector(`#current-circuit`)
+const clearButton = document.body.querySelector(`#clear-circuit`)
+clearButton.addEventListener(`click`, () => {
+    console.log(`i am cleared!`)
+    circuitList.innerText = ``
+})
+
+
+
+// ************CIRCUIT APPENDS, FIGUIRE OUT HOW TO GET SAVED CIRCUIT TO LIVE IN ITS OWN DIV
 const archiveButton = document.querySelector(`#save-circuit`)
 archiveButton.addEventListener(`click`, () => {
     console.log(`i am archived!`)
+    const circuitHistory = document.querySelector(`#circuit-history`)
+    const allWorkouts = document.querySelectorAll(`.current`)
+// LOOP THROUGH EACH ELEMENT IN NODE LIST AND APPEND TO CIRCUIT HISTORY
+    allWorkouts.forEach(workout => {
+        const oneCircuit = document.createElement(`ul`)
+        console.log(workout)
+        oneCircuit.appendChild(workout.cloneNode(true));
+        circuitHistory.appendChild(oneCircuit)
+        circuitList.innerText = ``
+
+        
+    })
+    
 })
 
 
-// CLEARS CURRENT CIRCUIT
-const clearButton = document.querySelector(`#clear-circuit`)
-clearButton.addEventListener(`click`, () => {
-    console.log(`i am cleared!`)
-    const circuitList = document.querySelector(`#current-circuit`)
-    circuitList.innerText = ``
-})
+
+
 
 
